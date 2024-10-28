ae80a87eb685f96fdfaf4b8a8d5a03a5f898495d
Arso991 <arsogn991@gmail.com>
2024-10-28 14:41:06 +0100

    about page start

--- resources/views/frontend/pages/widgets/about.blade.php
+++ resources/views/frontend/pages/widgets/about.blade.php
@@ -10,12 +10,13 @@
         <div class="row d-flex align-items-center">
             <div class="col-lg-6">
                 <div class="about-text">
-                    <h3 class="title wow fadeInUp" data-wow-delay="0.3s">
-                        {{ 'De la construction à la finition' }}</h3>
+                    {{-- <h3 class="title wow fadeInUp" data-wow-delay="0.3s">
+                        {{ 'De la construction à la finition' }}</h3> --}}
                     <div class="desc wow fadeInUp" data-wow-delay="0.4s">
-                        {!! 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi ut optio dolore ex minus tenetur voluptatum. Minus, placeat.' !!}
+                        {!! 'Fondé en 2022, le Laboratoire GAIA est un établissement géotechnique novateur basé au Bénin, dédié à l’analyse approfondie des sols et des matériaux de construction. Notre mission est de fournir des services de qualité qui garantissent la sécurité et la durabilité des projets d\'infrastructure dans notre pays et au-delà. <br/> GAIA se distingue par son engagement envers l’excellence et l’innovation. Notre équipe d\'experts, formée aux dernières techniques d\'analyse, réalise une vaste gamme d\'essais géotechniques, allant des études...' !!}
                     </div>
-                    <a href="" class="button-primary-trans mouse-dir wow fadeInUp" data-wow-delay="0.5s">
+                    <a href="{{ route('about') }}" class="button-blue-trans mouse-dir wow fadeInUp"
+                        data-wow-delay="0.5s">
                         <span class="text">En savoir plus</span>
                         <span class="dir-part"></span>
                     </a>
