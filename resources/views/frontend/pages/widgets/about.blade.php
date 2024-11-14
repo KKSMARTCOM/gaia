40f5db7e551175a9dfefba725e4ba43756e3facf
Arso991 <arsogn991@gmail.com>
2024-11-14 11:12:51 +0100

    about styles

--- resources/views/frontend/pages/widgets/about.blade.php
+++ resources/views/frontend/pages/widgets/about.blade.php
@@ -7,28 +7,25 @@
                 </div>
             </div>
         </div>
-        <div class="row d-flex align-items-center">
+        <div class="row ">
             <div class="col-lg-6 mb-4">
                 <div class="about-text">
-                    <h4 class="title wow fadeInUp" data-wow-delay="0.2s">
-                        {{ $about->title }}</h4>
                     <div class="desc wow fadeInUp" data-wow-delay="0.4s">
-                        {!! Str::limit($about->description, 100, '...') !!}
+                        {!! Str::limit($about->description, 500, '...') !!}
                     </div>
-                    <a href="{{ route('show.about', $about->id) }}" class="button-blue-trans mouse-dir wow fadeInUp"
-                        data-wow-delay="0.5s">
+                    <a href="{{ route('show.about', $about->id) }}"
+                        class="button-blue-trans mt-4 mouse-dir wow fadeInUp" data-wow-delay="0.5s">
                         <span class="text">En savoir plus</span>
                         <span class="dir-part"></span>
                     </a>
                 </div>
             </div>
-            <div class="col-lg-6 mb-4">
-                <figure class="about-image image-container">
-                    <img src="{{ asset($about->image) }}" alt="{{ $about->title }}"
-                        class="wow fadeInUp image-responsive" data-wow-delay="0.3s">
-                </figure>
-            </div>
-
+        </div>
+        <div class="col-lg-6 mb-4">
+            <figure class="about-image image-container">
+                <img src="{{ asset($about->image) }}" alt="{{ $about->title }}" class="wow fadeInUp image-responsive"
+                    data-wow-delay="0.3s">
+            </figure>
         </div>
     </div>
 </section>
